f74c202305658fdd5e3c4a8d17c9b838e7f879f5
PrashantRaj18198 <prashant@argonaut.dev>
2024-09-17 23:15:39 +0530

    Sync from local

--- lvim/config.lua
+++ lvim/config.lua
@@ -70,26 +70,26 @@ lvim.plugins = {
     dependencies = { "nvim-lua/plenary.nvim" },
     opts = {}
   },
-  {
-    "ruifm/gitlinker.nvim",
-    event = "BufRead",
-    config = function()
-      require("gitlinker").setup {
-        opts = {
-          -- remote = 'github', -- force the use of a specific remote
-          -- adds current line nr in the url for normal mode
-          add_current_line_on_normal_mode = true,
-          -- callback for what to do with the url
-          action_callback = require("gitlinker.actions").open_in_browser,
-          -- print the url after performing the action
-          print_url = false,
-          -- mapping to call url generation
-          mappings = "<leader>gy",
-        },
-      }
-    end,
-    dependencies = "nvim-lua/plenary.nvim",
-  },
+  -- {
+  --   "ruifm/gitlinker.nvim",
+  --   event = "BufRead",
+  --   config = function()
+  --     require("gitlinker").setup {
+  --       opts = {
+  --         -- remote = 'github', -- force the use of a specific remote
+  --         -- adds current line nr in the url for normal mode
+  --         add_current_line_on_normal_mode = true,
+  --         -- callback for what to do with the url
+  --         action_callback = require("gitlinker.actions").open_in_browser,
+  --         -- print the url after performing the action
+  --         print_url = false,
+  --         -- mapping to call url generation
+  --         mappings = "<leader>gy",
+  --       },
+  --     }
+  --   end,
+  --   dependencies = "nvim-lua/plenary.nvim",
+  -- },
   {
     "f-person/git-blame.nvim",
     event = "BufRead",
@@ -98,10 +98,10 @@ lvim.plugins = {
       require("gitblame").setup { enabled = false }
     end,
   },
-  {
-    "sindrets/diffview.nvim",
-    event = "BufRead",
-  }
+  -- {
+  --   "sindrets/diffview.nvim",
+  --   event = "BufRead",
+  -- }
 }
 
 lvim.builtin.nvimtree.active = false
