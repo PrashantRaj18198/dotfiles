03b4467e27204e31a45468860555f2f6c91e137c
PrashantRaj18198 <prashant@argonaut.dev>
2024-09-22 21:00:39 +0530

    Sync from local

--- lvim/config.lua
+++ lvim/config.lua
@@ -219,6 +219,9 @@ lvim.plugins = {
     opts = {
       suppressed_dirs = { '~/', '~/Projects', '~/Downloads', '/' },
       -- log_level = 'debug',
+      auto_session_enabled = true,
+      auto_save_enabled = true,
+      auto_restore_enabled = true,
     }
   }
 }
